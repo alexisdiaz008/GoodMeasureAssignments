2621fc084f73c0c20f5dc633cee6e790eff633d1
Rachel Metheny <rachelmetheny@Rachel-Methenys-MacBook-Pro.local>
2015-05-04 23:21:53 -0400

    controller and index pg setup

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,12 @@
 class HomeController < ApplicationController
+  
   def index
-  	
   	access_token = "594400459.1677ed0.5181455d8dc54c228a0377ad75f09774"
   	tag = "miamitech"
   	# asking rest_client to get post request from this address
   	@tag_result = RestClient.get "https://api.instagram.com/v1/tags/#{tag}/media/recent?access_token=#{access_token}"
-  
+	@tag_result = JSON.parse(@tag_result)
+  	#parsing @tag_results post request 
+  	
   end
 end
