c04ef73008abe2170771f4fd67e47b993a1a1e6c
Rachel Metheny <rachelmetheny@Rachel-Methenys-MacBook-Pro.local>
2015-05-04 22:58:35 -0400

    set root

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  # get 'home/index'
+  # root makes index.html our homepage
+  root "home#index"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
